d3386cdb3a0aa8d13a8ce2f965656cd25f8822d7
Chilaka Chinedu <greatchinex@gmail.com>
2019-06-12 16:36:10 +0100

    Updates

---
+++ precache-manifest.15a96928fd9d0080d49eeb765eee3655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295c40904e7bc1778d9633b95b2fe2e2",
+    "url": "/lyrics-search-app/index.html"
+  },
+  {
+    "revision": "040679917cca2de76d62",
+    "url": "/lyrics-search-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "846ea2d1b1b8924d63d4",
+    "url": "/lyrics-search-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "040679917cca2de76d62",
+    "url": "/lyrics-search-app/static/js/2.0f7d3f10.chunk.js"
+  },
+  {
+    "revision": "846ea2d1b1b8924d63d4",
+    "url": "/lyrics-search-app/static/js/main.d75ff4bd.chunk.js"
+  },
+  {
+    "revision": "c29adf8ae9ad7edfd37b",
+    "url": "/lyrics-search-app/static/js/runtime~main.61add989.js"
+  }
+]);
